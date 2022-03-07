52c433ca6af67f8c1dd6bb27d9fdc2e966c431b3
stephen-shopopop <94382341+stephen-shopopop@users.noreply.github.com>
2022-03-07 17:29:54 +0100

    test(index): add test sample

---
+++ src/index.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from '@jest/globals'
+
+function hello (value: string): string {
+  return 'hello ' + value
+}
+
+describe('[index/hello] hello()', () => {
+  test.each`
+    string | expected
+    ${'Shopopop'} | ${'hello Shopopop'}
+  `('returns $expected when hello $string', ({ string, expected }) => {
+    expect(hello(string)).toEqual(expected)
+  })
+})
